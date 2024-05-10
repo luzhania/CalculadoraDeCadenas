aeec81de6177ba793baa637ecbdc52b4de2e8a3a
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-10 14:47:02 -0400

    Refactoring function sumString

--- src/sumString.js
+++ src/sumString.js
@@ -1,13 +1,22 @@
-function sumString(stringOfNumbers) {
-  if (stringOfNumbers === "") {
-    return 0;
-  }
-  stringOfNumbers = stringOfNumbers.replace(/-/g, ',');
-  let numbers = stringOfNumbers.split(",");
+function formatString(stringOfNumbers) {
+  stringOfNumbers = stringOfNumbers.replace(/-/g, ",");
+  return stringOfNumbers.split(",");
+}
+
+function sumNumbers(numbers) {
   let sum = 0;
   for (let actualNumber = 0; actualNumber < numbers.length; actualNumber++) {
     sum += parseInt(numbers[actualNumber]);
   }
+  return sum;
+}
+
+function sumString(stringOfNumbers) {
+  if (stringOfNumbers === "") {
+    return 0;
+  }
+  let numbers = formatString(stringOfNumbers);
+  let sum = sumNumbers(numbers);
   return sum;
 }
 
